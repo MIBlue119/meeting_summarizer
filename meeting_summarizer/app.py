c3a109e4d297e6c988375c68d55a8c7a719eecc1
MIBlue119 <miblue119@gmail.com>
2023-03-05 11:22:00 +0800

    Feat: support multiple target languages to summary

--- meeting_summarizer/app.py
+++ meeting_summarizer/app.py
@@ -4,6 +4,7 @@ from meeting_summarizer.fileloader.webvtt_loader import WebVttLoader
 from meeting_summarizer.prompter import SummarizerPrompter
 from meeting_summarizer.config import AppConfig
 from meeting_summarizer.summarizer import Summarizer
+from meeting_summarizer.utils import LANGUAGES, TO_LANGUAGE_CODE
 import argparse
 
 def main(args):
@@ -13,6 +14,8 @@ def main(args):
     webvtt_loader = WebVttLoader()
     # Initialize the config class
     config = AppConfig()
+    config.set_text_engine(args.text_engine)
+    config.LANGUAGE = args.language
     if args.test:
         config.IS_TEST = True
         config.TEST_NUM = args.test_num
@@ -36,7 +39,21 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file_path", type=str, help="Path to the file to be summarized.")
+    parser.add_argument("--file_path", dest="file_path", type=str, help="Path to the file to be summarized.")
+    parser.add_argument(
+        "--text_engine", 
+        dest="text_engine",
+        type=str,default="gpt-3.5-turbo",
+        choices=["gpt-3.5-turbo","text-davinci-003"],
+        help="Text engine to be used for summarization.")
+    parser.add_argument(
+        "--language",
+        type=str,
+        choices=sorted(LANGUAGES.keys())
+        + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]),
+        default="zh-hant",
+        help="language to translate to",
+    )    
     parser.add_argument(
         "--test",
         dest="test",
@@ -47,8 +64,8 @@ if __name__ == "__main__":
         "--test_num",
         dest="test_num",
         type=int,
-        default=6,
+        default=4,
         help="test num for the test",
-    )    
+    )        
     args = parser.parse_args()
     main(args)
--- meeting_summarizer/prompter.py
+++ meeting_summarizer/prompter.py
@@ -2,17 +2,11 @@
 
 class SummarizerPrompter:
     def __init__(self, language):
-        if language not in ["en", "zh-tw"]:
-            print("Currently only supports en and zh-TW.")
-            raise ValueError(f"Invalid language: {language}")
         self.language = language
 
     def get_chunk_prompt(self, chunk, text_engine="text-davinci-003"):
-        if self.language == "en":
-            chunk_prompt = f"{chunk}\nTL;DR en:"
-        elif self.language == "zh-tw":
-            chunk_prompt = f"{chunk}\nTL;DR zhtw:"
-
+        chunk_prompt = f"#Input\n{chunk}\n#lang:{self.language}#Instuctions:TL;DR:"
+        print(chunk_prompt)
         if "text" in text_engine:
             return {
                 "prompt": chunk_prompt,
@@ -25,10 +19,7 @@ class SummarizerPrompter:
             }
         return chunk_prompt
     def get_consolidated_prompt(self, chunk_responses, text_engine="text-davinci-003"):
-        if self.language == "en":
-            consolidated_prompt = f"#lang:en#Instructions\n Consoloidate these summaries under 150 words.And list 5key takeaways.\n#Input\n{chunk_responses}"
-        elif self.language == "zh-tw":
-            consolidated_prompt = f"#lang:zh-tw#Instructions\n Consoloidate these summaries under 150 words.And list 5key takeaways.\n#Input\n{chunk_responses}"
+        consolidated_prompt = f"#Input\n{chunk_responses}\n#lang:{self.language}\n#Instructions\n Consoloidate these summaries under 150 words.And list 5key takeaways."
         if "text"in text_engine:
             return {
                 "prompt": consolidated_prompt,
